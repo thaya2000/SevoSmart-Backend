7a2a91be702739b886f74e8ba511bd0d9ba27006
Thayanan Thevarasa <thevarasathayanan@outlook.com>
2024-03-26 19:04:25 +0530

    first commit

---
+++ src/main/java/org/sevosmart/com/sevosmartbackend/SevoSmartBackendApplication.java
@@ -0,0 +1,13 @@
+package org.sevosmart.com.sevosmartbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SevoSmartBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SevoSmartBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sevosmart/com/sevosmartbackend/SevoSmartBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sevosmart.com.sevosmartbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SevoSmartBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
