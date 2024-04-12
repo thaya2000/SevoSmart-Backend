33d9baedd814286d95a21d5d47b22c0eef538cb7
Thayanan Thevarasa <thevarasathayanan@outlook.com>
2024-04-12 13:14:10 +0530

    configuration changes

--- src/main/java/org/sevosmart/com/sevosmartbackend/service/NotificationService.java
+++ src/main/java/org/sevosmart/com/sevosmartbackend/service/NotificationService.java
@@ -1,8 +1,7 @@
 package org.sevosmart.com.sevosmartbackend.service;
 
 import org.sevosmart.com.sevosmartbackend.model.Guest;
-import org.springframework.scheduling.annotation.Async;
-
+// import org.springframework.scheduling.annotation.Async;
 
 public interface NotificationService {
     void sendConsultationNotification(Guest guest);
